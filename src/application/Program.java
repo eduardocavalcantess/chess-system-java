156032d4802ca8d9219972ea26fc54a3b92f60f7
Eduardo Cavalcante <eduardo.cavalcantess@gmail.com>
2021-06-16 16:25:16 -0300

    Classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-
+		
+		Position p = new Position(1, 2);
+		System.out.println(p);
 	}
 
 }
